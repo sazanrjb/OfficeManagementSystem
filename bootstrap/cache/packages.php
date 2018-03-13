3c7cea27310a5ddca829465441b535577dd67878
Sajan Rajbhandari <sazanrjb@gmail.com>
2018-03-13 22:40:51 +0545

    Upgrade to laravel 5.5

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,14 @@
+<?php return array (
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+);
